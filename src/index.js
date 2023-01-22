c674985f3d3644006f532dc6f87fa803fc186b43
AnzorSv <a.svanishvili@gmail.com>
2023-01-22 14:27:50 +0400

    addes Variable.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Navbar from './Navbar';
+import './Variables.css'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
