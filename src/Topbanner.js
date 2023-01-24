9031d9faae74eb83f66f6dc78ec1dfd7349b1a1d
senses <a.svanishvili@gmail.com>
2023-01-24 16:22:35 +0400

    added banners

--- src/Topbanner.js
+++ src/Topbanner.js
@@ -2,7 +2,37 @@ import './Topbanner.css';
 
 function Topbanner() {
     return (
-        <div className='top-banner'></div>
+        <>
+        {/* <div className='top-banner'>
+            <div className='row-30'>
+                <div className='banner-one'></div>
+                <div className='banner-one'></div>
+            </div>
+            <div className='row-30'>
+                <div className='banner-one'></div>
+            </div>
+            <div className='row-30'>
+                <div className='banner-one'></div>
+            </div>
+        </div> */}
+
+        <div className='container'>
+            <div className='container-inner'>
+                  <div className='banner-one'></div>
+                  <div className='banner-one'></div>
+                  <div className='banner-one'></div>
+            </div>
+            {/* <div className='banner-one'>
+                <div style={{
+                    backgroundColor: "red",
+                    width1: "100%",
+                    height: "50px"
+                    }}></div>
+            </div> */}
+            {/* <div className='banner-one'>dsds</div>
+            <div className='banner-one'>dsds</div> */}
+        </div>
+        </>
     );
 }
 
