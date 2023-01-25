88791e3ae6938f15742f1fad1d44f7532d8be496
senses <a.svanishvili@gmail.com>
2023-01-25 17:57:18 +0400

    added grid in banners

--- src/Topbanner.js
+++ src/Topbanner.js
@@ -8,12 +8,57 @@ import Bannerseven from './banners/Bannerseven';
 import Bannereight from './banners/Bannereight';
 import './Topbanner.css';
 
+
+let banners = [
+    {
+        id: 1,
+        title: 'Banner1',
+        backgroundImg: 'https://i.picsum.photos/id/638/300/300.jpg?hmac=zK8CIKFdSqCYNZhdN5js6S4r6-UxVGSJ2HlT70Bp3og'
+    },
+    {
+        id: 2,
+        title: 'Banner2',
+        backgroundImg: 'https://i.picsum.photos/id/638/300/300.jpg?hmac=zK8CIKFdSqCYNZhdN5js6S4r6-UxVGSJ2HlT70Bp3og'
+    },
+    {
+        id: 3,
+        title: 'Banner3',
+        backgroundImg: 'https://i.picsum.photos/id/638/300/300.jpg?hmac=zK8CIKFdSqCYNZhdN5js6S4r6-UxVGSJ2HlT70Bp3og'
+    },
+    {
+        id: 4,
+        title: 'Banner4',
+        backgroundImg: 'https://i.picsum.photos/id/638/300/300.jpg?hmac=zK8CIKFdSqCYNZhdN5js6S4r6-UxVGSJ2HlT70Bp3og'
+    },
+    {
+        id: 5,
+        title: 'Banner5',
+        backgroundImg: 'https://i.picsum.photos/id/638/300/300.jpg?hmac=zK8CIKFdSqCYNZhdN5js6S4r6-UxVGSJ2HlT70Bp3og'
+    },
+    {
+        id: 6,
+        title: 'Banner6',
+        backgroundImg: 'https://i.picsum.photos/id/638/300/300.jpg?hmac=zK8CIKFdSqCYNZhdN5js6S4r6-UxVGSJ2HlT70Bp3og'
+    },
+    {
+        id: 7,
+        title: 'Banner7',
+        backgroundImg: 'https://i.picsum.photos/id/638/300/300.jpg?hmac=zK8CIKFdSqCYNZhdN5js6S4r6-UxVGSJ2HlT70Bp3og'
+    },
+    {
+        id: 8,
+        title: 'Banner8',
+        backgroundImg: 'https://i.picsum.photos/id/638/300/300.jpg?hmac=zK8CIKFdSqCYNZhdN5js6S4r6-UxVGSJ2HlT70Bp3og'
+    }
+];
+console.log(banners[0].id)
+
 function Topbanner() {
     return (
-        <div className='px-20'>
+        <div className='md:px-20 sm:p-4'>
         <div className='md:p-10  bg-sky-50' style={{borderRadius:'30px'}}>
-            <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6">
-                <Bannerone></Bannerone>
+            <div className="grid lg:grid-cols-3 md:grid-cols-1 sm:grid-cols-1 gap-6">
+                <Bannerone title={banners[0].title}></Bannerone>
                 <Bannertwo></Bannertwo>
                 <Bannerthree></Bannerthree>
                 <Bannertfour></Bannertfour>
@@ -24,6 +69,7 @@ function Topbanner() {
             </div>
         </div>
         </div>
+        
     );
 }
 
--- src/banners/Bannerone.js
+++ src/banners/Bannerone.js
@@ -1,6 +1,16 @@
-function Bannerone() {
+import shoesimg from '../img/shoes.png';
+
+function Bannerone(props) {
     return (
-        <div className='banner-one'>ბანერი 1</div>
+        <div className='banner-one px-10 text-white flex md:flex-row sm:flex-col justify-between items-center'>
+            <div>
+                <p>{props.title}</p>
+                <p>და მიიიღე მეორე</p>
+                <h1 style={{fontSize: '36px', fontWeight: 800}}>საჩუქრად</h1>
+                <div className='bg-white text-black px-4 py-2 rounded-full inline-block'>იყიდე ახლავე</div>
+            </div>
+            <img src={shoesimg}></img>
+        </div>
     );
 }
 
